b0e2a01b79262cf1ca8691c1e1d60fcbbd822478
Leandro Ventura <leandroventur19@gmail.com>
2026-01-27 09:04:36 -0300

    chore: bootstrap Inertia + vue 3

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  message: String,
+})
+</script>
+
+<template>
+  <div style="padding: 2rem">
+    <h1>Inertia + Vue 3</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
