b5e6747ccb014a39c137929db5ba931fd2fa4a1b
Leandro Ventura <leandroventur19@gmail.com>
2026-01-28 20:47:54 -0300

    feat(auth): implement login page with controller, route, and styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import '../css/weatherapp.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 
 createInertiaApp({
     title: title => `${title} - Laravel`,
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('login', [LoginController::class, 'index'])->name('login');
