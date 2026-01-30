0bc9da853531f41b7a03ff44a98f321707f37547
Leandro Ventura <leandroventur19@gmail.com>
2026-01-30 08:13:37 -0300

    refactor(auth): separate register and login modules
    Separate register and login in modules (Requests, Auth, Services)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,8 +11,4 @@ class LoginController extends Controller
     public function index(Request $request){
         return Inertia::render('Auth/Login');
     }
-
-    public function register(Request $request){
-        return Inertia::render('Auth/Register');
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -8,9 +9,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
-Route::get('register', [LoginController::class, 'register'])->name('register');
+Route::get('register', [RegisterController::class, 'create'])->name('register');
+Route::post('register', [RegisterController::class, 'store'])->name('register_submit');
 
 
 Route::group(['prefix' => 'dashboard'], function(){
-    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
