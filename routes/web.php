2d727f58302a5158a92bdceab901f998c69c6081
Leandro Ventura <leandroventur19@gmail.com>
2026-01-30 08:55:34 -0300

    feat(auth): implement login flow

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
+Route::post('login', [LoginController::class, 'submit'])->name('login_submit');
+Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
+
 Route::get('register', [RegisterController::class, 'create'])->name('register');
 Route::post('register', [RegisterController::class, 'store'])->name('register_submit');
 
 
 Route::group(['prefix' => 'dashboard'], function(){
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-});
+    Route::get('teste', function(){return session()->all();});
+})->middleware('auth');
